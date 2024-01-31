699b25214c5a8cb7135b99523de60263545208e5
NarutoUchiha39 <104666748+NarutoUchiha39@users.noreply.github.com>
2024-01-31 13:10:57 +0530

    Update index.mjs

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -41,7 +41,7 @@ app.get("/Cron-Check-Hourly",async(req,res)=>{
         let cur_count = result.in
         let cur_count_busiest = result.busiest_hour_count
         if(cur_count > cur_count_busiest){
-            Count.insertMany({in:result.in,out:result.out,teacher:result.teacher,student:result.student,unknown:result.unknown,busiest_hour:String(resu.hour),busiest_day:result.busiest_day,busiest_hour_count:cur_count})
+            Count.update({date:date_month},{$set:{busiest_hour:String(resu.hour)})
             
             return res.json({"modified":true}).status(200)        
         }else{
